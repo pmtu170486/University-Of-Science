797cb444501de2c0de8e586d3038c91853ceae9e
pmtu <pmtu@fit.hcmus.edu.vn>
2017-04-29 13:57:38 +0700

    Added

---
+++ CD/PTUD CSDL 2/Demos/QuanLyHangHoa/src/java/dto/SanPham.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Pham Minh Tu
+ */
+public class SanPham implements Serializable{
+    private int id;
+    private String tensp;
+    private String quycach;
+    private BigDecimal gia;    
+    private String nhasx;
+
+    public SanPham() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTensp() {
+        return tensp;
+    }
+
+    public void setTensp(String tensp) {
+        this.tensp = tensp;
+    }
+
+    public String getQuycach() {
+        return quycach;
+    }
+
+    public void setQuycach(String quycach) {
+        this.quycach = quycach;
+    }
+
+    public BigDecimal getGia() {
+        return gia;
+    }
+
+    public void setGia(BigDecimal gia) {
+        this.gia = gia;
+    }
+
+    public String getNhasx() {
+        return nhasx;
+    }
+
+    public void setNhasx(String nhasx) {
+        this.nhasx = nhasx;
+    }
+}
+
---
+++ CD/PTUD CSDL 2/Demos/QuanLyHangHoa/web/admin/SanPham.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : SanPham
+    Created on : Apr 29, 2017, 1:51:24 PM
+    Author     : home
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="才" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+
+    </body>
+</html>
